a2d82bfe92b62136b719c5951279d62a55827a5c
David Chin <dlcmhd@me.com>
2021-09-17 12:11:53 +0800

    feature

---
+++ 2021-09-17 nim-by-example.github.io/02. Variables/mutable_with_string_interpolation.nim
@@ -0,0 +1,26 @@
+# Mutable variables with string interpolation
+
+var
+  a = "foo"
+  b = 10
+
+  # Works fine, initialized to 0
+  c: int
+
+# https://nim-lang.org/docs/strformat.html
+import strformat
+
+echo "\nwith `strformat`"
+
+echo "a: {a}".fmt
+echo "b: {b}".fmt
+echo "c: {c}".fmt
+
+# https://stackoverflow.com/questions/58739226/how-do-you-do-string-interpolation-with-variables-in-nim
+import strutils
+
+echo "\nwith `strutils`"
+
+let message = "$#, $#, $#"
+let interpolation = message % [a, "{b}".fmt, "{c}".fmt]
+echo interpolation
