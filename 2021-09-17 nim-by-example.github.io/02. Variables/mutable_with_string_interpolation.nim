b81b6caafdadda93611034a168a890cc855f5a63
David Chin <dlcmhd@me.com>
2021-09-17 12:55:23 +0800

    feature

--- 2021-09-17 nim-by-example.github.io/02. Variables/mutable_with_string_interpolation.nim
+++ 2021-09-17 nim-by-example.github.io/02. Variables/mutable_with_string_interpolation.nim
@@ -10,7 +10,7 @@ var
 # https://nim-lang.org/docs/strformat.html
 import strformat
 
-echo "\nwith `strformat`"
+echo "\nwith `strformat` - before mutation"
 
 echo "a: {a}".fmt
 echo "b: {b}".fmt
@@ -24,7 +24,7 @@ c = 3
 # https://stackoverflow.com/questions/58739226/how-do-you-do-string-interpolation-with-variables-in-nim
 import strutils
 
-echo "\nwith `strutils`"
+echo "\nwith `strutils` - after mutation"
 
 let message = "$#, $#, $#"
 let interpolation = message % [a, "{b}".fmt, "{c}".fmt]
