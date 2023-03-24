edd74573e0b8f3160dad0931030433bef528f51d
David Chin <dlcmhd@me.com>
2023-03-24 13:06:23 +0800

    inheritance

--- nim-tutorial-2/2.object-oriented-programming/inheritance.nim
+++ nim-tutorial-2/2.object-oriented-programming/inheritance.nim
@@ -1,3 +1,34 @@
 type
-  Person = ref object of RootObj # `RootObj` enables inheritance with runtime type information
+  # `RootObj` enables inheritance with runtime type information
+  Person = ref object of RootObj
     name*: string # `*` allows other modules to access `name`
+    age: int      # no `*` means that this field is hidden from other modules
+
+                  # Student inherits from Person with an `id` field
+  Student = ref object of Person
+    id: int
+
+var
+  student: Student
+  person: Person
+
+assert student of Student # Hint: condition is always true
+
+# object construction
+student = Student(name: "Anton", age: 5, id: 2)
+
+# echo student
+# Error: type mismatch: got <Student>
+# but expected one of:
+# proc echo(x: varargs[typed, `$`])
+#   first type mismatch at position: 1
+#   required type for x: varargs[typed]
+#   but expression 'student' is of type: Student
+
+echo repr student
+# ref 0x10cd05050 --> [id = 2,
+# name = 0x10cd05090"Anton",
+# age = 5]
+
+echo student[]
+# (id: 2, name: "Anton", age: 5)
