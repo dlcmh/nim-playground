7a6b9c13f83c1832b7ab7342218939975843791e
David Chin <dlcmhd@me.com>
2023-04-22 17:14:45 +0800

    prime factors

---
+++ exercism/prime_factors.nim
@@ -0,0 +1,53 @@
+proc primeFactors*(n: int64): seq[int] =
+  var
+    n = n
+    divisor = 2
+  while divisor <= n:
+    if n mod divisor == 0:
+      result.add divisor
+      n = n div divisor
+    else:
+      if divisor >= 3: # optimization: odd divisors only after 3
+        divisor += 2
+      else:
+        divisor.inc
+
+when isMainModule:
+  import unittest
+
+  suite "Prime Factors":
+    test "no factors":
+      check primeFactors(1).len == 0
+
+    test "prime number":
+      check primeFactors(2) == @[2]
+
+    test "another prime number":
+      check primeFactors(3) == @[3]
+
+    test "square of a prime":
+      check primeFactors(9) == @[3, 3]
+
+    test "product of first prime":
+      check primeFactors(4) == @[2, 2]
+
+    test "cube of a prime":
+      check primeFactors(8) == @[2, 2, 2]
+
+    test "product of second prime":
+      check primeFactors(27) == @[3, 3, 3]
+
+    test "product of third prime":
+      check primeFactors(625) == @[5, 5, 5, 5]
+
+    test "product of first and second prime":
+      check primeFactors(6) == @[2, 3]
+
+    test "product of primes and non-primes":
+      check primeFactors(12) == @[2, 2, 3]
+
+    test "product of primes":
+      check primeFactors(901_255) == @[5, 17, 23, 461]
+
+    test "factors include a large prime":
+      check primeFactors(93_819_012_551) == @[11, 9_539, 894_119]
