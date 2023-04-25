e5ef8287e2cf30dbfa5d5948e5abc415e09056d4
David Chin <dlcmhd@me.com>
2023-04-25 17:56:09 +0800

    rotational cipher

---
+++ exercism/rotational_cipher.nim
@@ -0,0 +1,43 @@
+proc rotate*(s: string, n: int): string =
+  for c in s:
+    if c in 'a' .. 'z':
+      result.add 'a'.succ((n + {'a' .. 'z'}.find(c)) mod 26)
+    elif c in 'A' .. 'Z':
+      result.add 'A'.succ((n + {'A' .. 'Z'}.find(c)) mod 26)
+    else:
+      result.add c
+
+when isMainModule:
+  import unittest
+
+  suite "Rotational Cipher":
+    test "rotate a by 0, same output as input":
+      check rotate("a", 0) == "a"
+
+    test "rotate a by 1":
+      check rotate("a", 1) == "b"
+
+    test "rotate a by 26, same output as input":
+      check rotate("a", 26) == "a"
+
+    test "rotate m by 13":
+      check rotate("m", 13) == "z"
+
+    test "rotate n by 13 with wrap around alphabet":
+      check rotate("n", 13) == "a"
+
+    test "rotate capital letters":
+      check rotate("OMG", 5) == "TRL"
+
+    test "rotate spaces":
+      check rotate("O M G", 5) == "T R L"
+
+    test "rotate numbers":
+      check rotate("Testing 1 2 3 testing", 4) == "Xiwxmrk 1 2 3 xiwxmrk"
+
+    test "rotate punctuation":
+      check rotate("Let's eat, Grandma!", 21) == "Gzo'n zvo, Bmviyhv!"
+
+    test "rotate all letters":
+      check rotate("The quick brown fox jumps over the lazy dog.",
+                   13) == "Gur dhvpx oebja sbk whzcf bire gur ynml qbt."
