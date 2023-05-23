6552238415b77e2fb12cd7ea39f539ad497f57dc
David Chin <dlcmhd@me.com>
2023-05-23 15:15:59 +0800

    refactor

--- nim_in_action/parse_with_parseutils.nim
+++ nim_in_action/parse_with_parseutils.nim
@@ -2,30 +2,34 @@
 
 import std/[parseutils]
 
-let line = "en Nim_(programming_language) 1 70231"
+proc parsed*(line: string): (string, string, int, int) =
+  var pos = 0
 
-var pos = 0
+  var domainCode = ""
+  pos.inc parseUntil(line, domainCode, {' '}, pos)
 
-var domainCode = ""
-pos.inc parseUntil(line, domainCode, {' '}, pos)
+  pos.inc
 
-pos.inc
+  var pageTitle = ""
+  pos.inc parseUntil(line, pageTitle, {' '}, pos)
 
-var pageTitle = ""
-pos.inc parseUntil(line, pageTitle, {' '}, pos)
+  pos.inc
 
-pos.inc
+  var pageViews = 0
+  pos.inc parseInt(line, pageViews, pos)
 
-var pageViews = 0
-pos.inc parseInt(line, pageViews, pos)
+  pos.inc
 
-pos.inc
+  var pageSize = 0
+  pos.inc parseInt(line, pageSize, pos)
 
-var pageSize = 0
-pos.inc parseInt(line, pageSize, pos)
+  result = (domainCode, pageTitle, pageViews, pageSize)
 
 when isMainModule:
   import std/[unittest]
+
+  let (domainCode, pageTitle, pageViews,
+    pageSize) = "en Nim_(programming_language) 1 70231".parsed
 
   check domainCode == "en"
   check pageTitle == "Nim_(programming_language)"
